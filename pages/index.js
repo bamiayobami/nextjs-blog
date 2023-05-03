04fcf6f5460149c647eaa017df247421f90cd69f
bamiayobami <michaelaybm@gmail.com>
2023-05-03 17:22:31 +0100

    not important anyways, delete the changes later

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/* edit for vrecel git*/}
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
